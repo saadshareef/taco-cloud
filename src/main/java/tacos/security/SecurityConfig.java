271bd1d7ce139a711533a93bfa5eae1a3310ade2
Saad Shareef <saad.m.shareef@gmail.com>
2020-09-01 19:43:56 +0300

    project init

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("aaa").password("{noop}abc").authorities("ROLE_USER")
+			.and()
+			.withUser("bbb").password("{noop}bcd").authorities("ROLE_USER");		
+	}
+
+}
---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,19 @@
+package tacos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+	@GetMapping
+	public String showDesignForm() {
+		log.info("Getting Design Form.");
+		return "designForm";
+	}
+}
