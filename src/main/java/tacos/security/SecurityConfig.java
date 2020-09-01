234953ae99fbbafe2ff78070aa3b87c6b0ceac11
Saad Shareef <saad.m.shareef@gmail.com>
2020-09-01 21:03:58 +0300

    Add Security config

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package tacos.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +17,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.withUser("bbb").password("{noop}bcd").authorities("ROLE_USER");		
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/design").hasRole("USER")
+			.antMatchers("/", "/**").permitAll()
+//		.and()
+//			.formLogin().loginPage("/login")
+		;
+	}
+	
+	
 
 }
